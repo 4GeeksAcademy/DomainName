13fb8d43fa44da8633f39f045d8fe80f30016ff7
Marawan Elzoeiry <140724136+MarawanElzoeiry@users.noreply.github.com>
2023-10-03 11:45:29 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let dots = [".com", ".org", ".net"];
+
+  let domainNames = [];
+
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let dot of dots) {
+          domainNames.push(`${pronoun}${adj}${noun}${dot}`);
+        }
+      }
+    }
+  }
+
+  document.body.innerHTML = `<ul> ${domainNames
+    .map(domainName => `<li>${domainName}</li>`)
+    .join("")} </ul>`;
 };
